0a59c54b383e55596f5b3d2fe29d15efe5bc0cb7
github username <github email address>
2019-12-03 18:13:17 -0500

    Added Models, Set up some routes

--- Mooovies/app/controllers/reviews_controller.rb
+++ Mooovies/app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+    def index
+        @reviews = Review.all
+    end
+
+    def show
+        @review =  Review.find(params[:id])
+    end
+
+    def new
+
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.create(params.require(:review).permit(:rating, :content), movie: @movie, user: @user )
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:rating, :content, :user_id, :movie_id)
+    end
 end
--- Mooovies/config/routes.rb
+++ Mooovies/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :movies
-  resources :users
+  resources :reviews, only: [:index, :show, :new, :create, :edit, :update, :delete]
+  resources :movies, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
