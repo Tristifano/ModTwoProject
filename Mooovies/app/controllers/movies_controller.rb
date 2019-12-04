e2bb5b95ed210203461ca26fffdda31968436487
github username <github email address>
2019-12-04 09:29:48 -0500

    messed around with some search functionality. added api calls.

--- Mooovies/app/controllers/movies_controller.rb
+++ Mooovies/app/controllers/movies_controller.rb
@@ -1,2 +1,16 @@
 class MoviesController < ApplicationController
+    def index
+        
+    end
+
+    def show
+    end
+
+    private
+
+    def movie_api_call
+       response =  RestClient.get("http://www.omdbapi.com/?s=#{}&apikey=3f96a270")
+       results_hash = JSON.parse(response.body)
+       results_array =  results_hash[:Search]
+    end
 end
